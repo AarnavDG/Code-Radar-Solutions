997f6890821d220a5e4ba040a6bff88c3eecbc21
AarnavDG <duggalaarnav@gmail.com>
2025-01-20 19:19:20 +0530

    Solution to Check Even or Odd

--- Check Even or Odd@@6788dd63490637cfc44e1b8d/code.c
+++ Check Even or Odd@@6788dd63490637cfc44e1b8d/code.c
@@ -5,10 +5,10 @@ int main() {
     int x;
     scanf("%d",&x);
     if(x%2==0){
-        printf("Even")
+        printf("Even");
     }
     else if(x%2!=0){
-        printf("Odd")
+        printf("Odd");
     }
     return 0;
 }
