c9de4f461b3abf78d92e137e517c90fc5dfc7204
AarnavDG <duggalaarnav@gmail.com>
2025-01-21 15:24:54 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -4,10 +4,10 @@ int main() {
     int x,y;
     char a;
     scanf("%d %d %c",&x,&y,&a);
-    if(a==='+'){
+    if(a=='+'){
         printf("%d",x+y);
     }
-    else if(a='-'){
+    else if(a=='-'){
         printf("%d",x-y);
     }
     else if(x=='*'){
