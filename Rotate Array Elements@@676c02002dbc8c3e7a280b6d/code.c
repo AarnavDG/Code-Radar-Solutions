b646310ba36dc557abdb17ad56dd492c1b5e2f98
AarnavDG <duggalaarnav@gmail.com>
2025-02-10 09:55:00 +0530

    Solution to Rotate Array Elements

--- Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
+++ Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
@@ -34,7 +34,8 @@ int main() {
     rotateArray(arr, n, k);
     
     for (int i = 0; i < n; i++) { 
-        printf("%d ", arr[i]); 
+        printf("%d ", arr[i]);
+        printf("/n"); 
     }
 
     return 0;
